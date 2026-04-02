3556df529c2a3795f640b8ecef17f6bf62073932
torrua <torrua@gmail.com>
2026-04-03 00:53:46 +0300

    fix: use esnext target to avoid destructuring transform error

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-const host = process.env.TAURI_DEV_HOST;
-
 export default defineConfig({
   plugins: [svelte()],
   clearScreen: false,
   server: {
     port: 5173,
     strictPort: false,
-    host: host || false,
-    hmr: host ? { protocol: 'ws', host, port: 5183 } : undefined,
     watch: { ignored: ['**/src-tauri/**'] },
   },
   envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
-    target: process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
-    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
-    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+    target: 'esnext',
+    minify: true,
+    sourcemap: false,
   },
 });
